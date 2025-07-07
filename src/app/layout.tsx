a39e18878f9475657d6cd8ab113a32f48ce52f69
vantung <dovantung1000@gmail.com>
2025-07-07 22:05:04 +0700

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import ScrollBackground from "@/components/ScrollBackground";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "My Portfolio",
